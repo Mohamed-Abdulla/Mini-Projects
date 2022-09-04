287d4a40e58f2aa8c75b2094ba3530591b639b96
mohamed-abdulla <abdabdulla78@gmail.com>
2022-09-04 09:32:51 +0530

    link added

--- cart/src/pages/Home.tsx
+++ cart/src/pages/Home.tsx
@@ -2,17 +2,18 @@ import { Container, Stack } from "react-bootstrap";
 import img from "../../public/imgs/github.png";
 const Home = () => {
   return (
-    <Container>
-      <Stack>
+    <Container className="d-flex align-items-center justify-content-space-between">
+      <Stack gap={4}>
         <h1>Check the store tab</h1>
         <h5>This advanced shopping cart is build using typescript and react</h5>
       </Stack>
 
-      <Container>
-        <a href="" target="_blank" rel="no">
+      <div>
+        <span>Source code -- </span>
+        <a href="https://github.com/Mohamed-Abdulla/Mini-Projects/tree/cart" target="_blank" rel="">
           <img src={img} alt="github" />
         </a>
-      </Container>
+      </div>
     </Container>
   );
 };
